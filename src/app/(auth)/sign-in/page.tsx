239ef1ab3dffd469def8ad2a41009a5eccd3808e
Tushar75R <mylmye07@gmail.com>
2024-09-21 18:58:25 +0530

    ADD frontend of sign-up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,24 +1,6 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import Link from "next/link";
-import { useState } from "react";
-import { useDebounceValue } from "usehooks-ts";
-import { useToast } from "@/hooks/use-toast";
-import { useRouter } from "next/navigation";
+import React from "react";
 
 const page = () => {
-  const [username, setUsername] = useState("");
-  const [usernameMessage, setUsernameMessage] = useState("");
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const debouncedUsername = useDebounceValue(username, 300);
-  const { toast } = useToast();
-  const router = useRouter();
-
   return <div>page</div>;
 };
 
